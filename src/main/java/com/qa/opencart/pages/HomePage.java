c049e743971f691b7ee0b4a0d839d5494943d44a
kanyac <kanyachenniappan@gmail.com>
2021-01-17 17:11:28 -0600

    Made change to Home page

--- src/main/java/com/qa/opencart/pages/HomePage.java
+++ src/main/java/com/qa/opencart/pages/HomePage.java
@@ -1,9 +1,18 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class HomePage {
 	
+	
+	private By Home = By.tagName("Your Store");
 	public HomePage() {
 	
 	}
+	public void goToHomePage()
+	{
+		System.out.println("Clicking on Home Page ");
+	}
+	
  
 }
